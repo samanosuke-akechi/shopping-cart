fd17e4248ecb5974c1d9655c32f8fc52acdcf12a
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-06 12:57:30 +0900

    購入処理
    購入時にitem_ordersテーブルに商品のidを登録することで販売を管理する。
    購入したらトップにリダイレクト。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
+  
   def index
     @items = Item.all
+  end
+
+  def order
+    ItemOrder.create(item_id: params[:id])
+    redirect_to root_path
+  end
+
+  private
+
+  def find_item
+    Item.find(params[:id])
   end
 end
